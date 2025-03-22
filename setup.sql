f9176e864176fed4883b747a65a1517c5c77d7c6
Simone <simonesamardzhiev@gmail.com>
2025-03-22 20:53:45 +0200

    Add event data in setup.sql

--- setup.sql
+++ setup.sql
@@ -9,11 +9,28 @@ CREATE TABLE events
 (
     id          SERIAL PRIMARY KEY,
     title       VARCHAR(255) NOT NULL,
-    date        TIMESTAMP    NOT NULL,
+    date        DATE         NOT NULL,
     address     VARCHAR(255) NOT NULL,
     image_url   VARCHAR(255) NOT NULL,
     description TEXT         NOT NULL
 );
+
+INSERT INTO events(title, date, address, image_url, description)
+VALUES ('Доброволческа акция: Почисти природата',
+        '2025-04-15',
+        'Парк „Борисова градина“, София',
+        'https://i.postimg.cc/4xptQzXm/image1.avif',
+        'Присъединете се към нас в еднодневна инициатива за почистване на парка! Ще осигурим ръкавици и чували, а в края на деня ще има пикник за участниците.'),
+       ('Как да създаваме екологични навици?',
+        '2025-04-25',
+        'Зала „Грийн Лаб“, Пловдив',
+        'https://i.postimg.cc/TYVgrVdh/image2.avif',
+        'Практически семинар за изграждане на устойчиви навици в ежедневието – от намаляване на отпадъците до екологично пазаруване. Включени са интерактивни задачи и подаръци за участниците.'),
+       ('Фестивал на уличното изкуство',
+        '2025-05-05',
+        'Център на Варна',
+        'https://i.postimg.cc/dtPGkHKz/image3.avif',
+        ' Потопи се в света на уличното изкуство! Очакват те графити демонстрации, музика на живо и арт базар с местни творци.');
 
 CREATE TABLE users_events
 (
