ab00c4b5b436170b21ec6a1ea325f20beefb4337
Simone <simonesamardzhiev@gmail.com>
2025-03-24 23:10:57 +0200

    Add the function for getting events when not registered

--- static/assets/js/events.js
+++ static/assets/js/events.js
@@ -1,26 +1,57 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const token = sessionStorage.getItem("token");
-    let response;
 
+    if (token) {
+        await getRegisteredEvents(token)
+    } else {
+        await getEvents()
+    }
+});
+
+const getEvents = async () => {
     try {
-        if (token) {
-            response = await fetch("https://server-project-production-b671.up.railway.app/events/registered", {
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
-            });
-        } else {
-            response = await fetch("https://server-project-production-b671.up.railway.app/events");
-        }
-
+        const response = await fetch("https://server-project-production-b671.up.railway.app/events")
         if (!response.ok) {
-            console.error("Възникна грешка при рареждането на събитията")
+            console.error("Error getting events")
+            alert("Грешка при зараждането на събития")
         }
 
         const data = await response.json()
-        console.dir(data)
+        const eventsList = document.getElementById("events-list")
+
+        data.forEach((event) => {
+            const listItem = document.createElement("li")
+
+            const eventTitle = document.createElement("h1")
+            eventTitle.innerText = event.title;
+            listItem.appendChild(eventTitle)
+
+            const image = document.createElement("img")
+            image.src = event.image_url
+            image.alt = event.title
+            listItem.appendChild(image)
+
+            const eventDate = document.createElement("p")
+            eventDate.innerText = `Дата: ${new Date(event.date).toLocaleDateString()}`
+            listItem.appendChild(eventDate)
+
+
+            const eventAddress = document.createElement("p")
+            eventAddress.innerText = event.address
+            listItem.appendChild(eventAddress)
+
+            let eventDescription = document.createElement('p');
+            eventDescription.innerText = event.description;
+            listItem.appendChild(eventDescription)
+
+            eventsList.appendChild(listItem)
+        })
+
     } catch (error) {
         console.error(error)
-        alert("Възникна грешка при рареждането на събитията")
     }
-});
+}
+
+const getRegisteredEvents = async (token) => {
+
+}
