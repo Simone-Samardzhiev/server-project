ab6bbc4667e6fec4d2f8fe69323c5782b22eb9c5
Simone <simonesamardzhiev@gmail.com>
2025-03-24 23:21:32 +0200

    Add function for loading events when the user is logged in

--- static/assets/js/events.js
+++ static/assets/js/events.js
@@ -40,7 +40,7 @@ const getEvents = async () => {
             eventAddress.innerText = event.address
             listItem.appendChild(eventAddress)
 
-            let eventDescription = document.createElement('p');
+            const eventDescription = document.createElement('p');
             eventDescription.innerText = event.description;
             listItem.appendChild(eventDescription)
 
@@ -48,10 +48,56 @@ const getEvents = async () => {
         })
 
     } catch (error) {
+        alert("Грешка при зараждането на събития")
         console.error(error)
     }
 }
 
 const getRegisteredEvents = async (token) => {
+    try {
+        const response = await fetch("https://server-project-production-b671.up.railway.app/events/registered", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
 
+        const data = await response.json()
+
+        const eventsList = document.getElementById("events-list")
+
+        data.forEach((event) => {
+            const listItem = document.createElement("li")
+
+            const eventTitle = document.createElement("h1")
+            eventTitle.innerText = event.title;
+            listItem.appendChild(eventTitle)
+
+            const image = document.createElement("img")
+            image.src = event.image_url
+            image.alt = event.title
+            listItem.appendChild(image)
+
+            const eventDate = document.createElement("p")
+            eventDate.innerText = `Дата: ${new Date(event.date).toLocaleDateString()}`
+            listItem.appendChild(eventDate)
+
+
+            const eventAddress = document.createElement("p")
+            eventAddress.innerText = event.address
+            listItem.appendChild(eventAddress)
+
+            const eventDescription = document.createElement('p');
+            eventDescription.innerText = event.description;
+            listItem.appendChild(eventDescription)
+
+            const checkBox = document.createElement("input")
+            checkBox.type = "checkbox"
+            checkBox.checked = event.is_registered
+
+            eventsList.appendChild(listItem)
+        })
+    } catch (error) {
+        alert("Грешка при зараждането на събития")
+        console.error(error)
+    }
 }
