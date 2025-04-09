ba81c5e95c8aa804d8dc28427c5c66aa77f2bfc0
Simone <simonesamardzhiev@gmail.com>
2025-04-09 14:05:43 +0300

    Change cors allowed networks

--- utils/enable_cors.go
+++ utils/enable_cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func EnableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://server-project-production-b671.up.railway.app")
+		w.Header().Set("Access-Control-Allow-Origin", "https://perpetual-energy-production.up.railway.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
